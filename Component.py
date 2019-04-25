14e41aed432a3c7ab3a8c83fda5154eaf7c4c6f8
R-Andrei <faragau.rares@gmail.com>
2019-04-25 13:24:45 +0300

    refactoring

--- Component.py
+++ Component.py
@@ -1,10 +1,11 @@
 class Component(object):
+
     def __init__(self, component_name, component_value):
         if self.validate_name(component_name):
             self.name = component_name
             self.value = component_value
         else:
-            raise TypeError('Component name should be a string.')
+            self.return_invalid()
 
     def __eq__(self, other):
         return self.name == other.name and self.value == other.value
@@ -19,10 +20,13 @@ class Component(object):
         if self.validate_name(component_name):
             self.name = component_name
         else:
-            raise TypeError('Component name should be a string.')
+            self.return_invalid()
 
     def set_value(self, component_value):
         self.value = component_value
     
     def validate_name(self, component_name):
         return True if isinstance(component_name, str) else False
+
+    def return_invalid(self):
+        raise TypeError('Component name should not be a number.')
