bcc0f989e77b8702c456fe95789f1325a924d69f
R-Andrei <faragau.rares@gmail.com>
2019-05-17 18:04:42 +0300

    Bug fixes, transition to objects in rest of project

--- Component.py
+++ Component.py
@@ -9,7 +9,7 @@ class Component(object):
 
     def __eq__(self, other):
         return self.name == other.name and self.value == other.value
-    
+
     def __repr__(self):
         return self.name + ': ' + str(self.value) if self.value else self.name + ': ' + str(None)
 
@@ -22,11 +22,24 @@ class Component(object):
         else:
             self.return_invalid()
 
+    def __getitem__(self, item_name):
+        if item_name == 'name': return self.name if self.name else None
+        elif item_name == 'value': return self.value if self.value else None
+        return None
+
     def set_value(self, component_value):
         self.value = component_value
-    
+
     def validate_name(self, component_name):
         return True if isinstance(component_name, str) else False
 
     def return_invalid(self):
         raise TypeError('Component name should not be a number.')
+
+    def __value__(self):
+        return self.value if self.value else None
+
+    def exists(self):
+        if self.value:
+            return True
+        return False
